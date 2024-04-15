6933457e68bf668bf3767c4317b69b89d2ce73dc
nKadykov <nkadykov@gmail.com>
2024-04-15 21:33:30 +0300

    init

--- mymodel.cpp
+++ mymodel.cpp
@@ -1,5 +1,9 @@
 #include "mymodel.h"
 
+#include <QFont>
+#include <QBrush>
+#include <QDebug>
+
 MyModel::MyModel(QObject *parent) : QAbstractTableModel(parent) {}
 
 int MyModel::rowCount(const QModelIndex &) const {
@@ -11,8 +15,37 @@ int MyModel::columnCount(const QModelIndex &) const {
 }
 
 QVariant MyModel::data(const QModelIndex &index, int role) const {
-    if(role == Qt::DisplayRole) {
-        return QString("Row%1, Column%2").arg(index.row() + 1).arg(index.column() + 1);
+    int row = index.row();
+    int col = index.column();
+
+    qDebug() << QString("ro2 %1, col %2, role %3").arg(row).arg(col).arg(role);
+
+    switch(role) {
+    case Qt::DisplayRole:
+        if(row == 0 && col == 1) return QString("<--left");
+        if(row == 1 && col == 1) return QString("right->");
+
+        return QString("Row%1, Column%2").arg(row + 1).arg(col + 1);
+    case Qt::FontRole:
+        if(row == 0 && col == 0) {
+            QFont boldFont;
+            boldFont.setBold(true);
+            return boldFont;
+        }
+        break;
+    case Qt::BackgroundRole:
+        if(row == 1 && col == 2) {
+            return QBrush(Qt::red);
+        }
+        break;
+    case Qt::TextAlignmentRole:
+        if(row == 1 && col == 1)
+            return int(Qt::AlignRight | Qt::AlignVCenter);
+        break;
+    case Qt::CheckStateRole:
+        if(row == 1 && col == 0)
+            return Qt::Checked;
+        break;
     }
     return QVariant();
 }
