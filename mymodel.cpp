3b3300a739b7978155920eae3128dff74ef6649d
nKadykov <nkadykov@gmail.com>
2024-04-15 21:50:54 +0300

    init

--- mymodel.cpp
+++ mymodel.cpp
@@ -1,10 +1,12 @@
 #include "mymodel.h"
 
-#include <QFont>
-#include <QBrush>
-#include <QDebug>
+#include <QTime>
 
-MyModel::MyModel(QObject *parent) : QAbstractTableModel(parent) {}
+MyModel::MyModel(QObject *parent) : QAbstractTableModel(parent), timer(new QTimer(this)) {
+    timer->setInterval(1000);
+    connect(timer, &QTimer::timeout, this, &MyModel::timerHit);
+    timer->start();
+}
 
 int MyModel::rowCount(const QModelIndex &) const {
     return 2;
@@ -18,34 +20,14 @@ QVariant MyModel::data(const QModelIndex &index, int role) const {
     int row = index.row();
     int col = index.column();
 
-    qDebug() << QString("ro2 %1, col %2, role %3").arg(row).arg(col).arg(role);
+    if(role == Qt::DisplayRole && row == 0 && col == 0)
+        return QTime::currentTime().toString();
 
-    switch(role) {
-    case Qt::DisplayRole:
-        if(row == 0 && col == 1) return QString("<--left");
-        if(row == 1 && col == 1) return QString("right->");
-
-        return QString("Row%1, Column%2").arg(row + 1).arg(col + 1);
-    case Qt::FontRole:
-        if(row == 0 && col == 0) {
-            QFont boldFont;
-            boldFont.setBold(true);
-            return boldFont;
-        }
-        break;
-    case Qt::BackgroundRole:
-        if(row == 1 && col == 2) {
-            return QBrush(Qt::red);
-        }
-        break;
-    case Qt::TextAlignmentRole:
-        if(row == 1 && col == 1)
-            return int(Qt::AlignRight | Qt::AlignVCenter);
-        break;
-    case Qt::CheckStateRole:
-        if(row == 1 && col == 0)
-            return Qt::Checked;
-        break;
-    }
     return QVariant();
+}
+
+void MyModel::timerHit() {
+    QModelIndex topLeft = createIndex(0, 0);
+
+    emit dataChanged(topLeft, topLeft, {Qt::DisplayRole});
 }
--- mymodel.h
+++ mymodel.h
@@ -2,15 +2,23 @@
 #define MYMODEL_H
 
 #include <QAbstractTableModel>
+#include <QTimer>
 
 class MyModel : public QAbstractTableModel
 {
     Q_OBJECT
 public:
     explicit MyModel(QObject *parent = nullptr);
+
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private slots:
+    void timerHit();
+
+private:
+    QTimer *timer;
 };
 
 #endif // MYMODEL_H
